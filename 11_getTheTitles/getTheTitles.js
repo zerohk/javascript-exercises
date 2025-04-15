e9539a6080d6d8c06c99d2f6b1e65327b654dbfb
buercheng-linux <www.1245731267@qq.com>
2025-04-15 14:04:38 +0800

    finished 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,18 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(obj) {
+  return obj.map((book) => book.title)
 };
+
+console.log(getTheTitles([
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]));
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
