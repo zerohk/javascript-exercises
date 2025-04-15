bb0611efe465fefc998decf672ad99a68eb78b44
buercheng-linux <www.1245731267@qq.com>
2025-04-15 13:59:29 +0800

    finished 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function (nth) {
+  if (nth < 0) return 'OOPS';
+  if (nth == 0) return 0;
+  if (nth == 1 || nth == 2) return 1;
+  return fibonacci(nth - 1) + fibonacci(nth - 2);
 };
+
+console.log(fibonacci(4));
+
 
 // Do not edit below this line
 module.exports = fibonacci;
