243ada443f8140b926956958651e1bda132a59fa
buercheng-linux <www.1245731267@qq.com>
2025-04-15 13:13:24 +0800

    finished 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,44 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((sum, value) => sum += value, 0);
 };
 
-const multiply = function() {
+// console.log(sum([7, 11]));
 
+
+const multiply = function (arr) {
+  return arr.reduce((production, value) => production *= value, 1);
 };
 
-const power = function() {
-	
+// console.log(multiply([2, 4]));
+
+
+const power = function (a, b) {
+  let result = 1;
+  for (let i = 0; i < b; i++) {
+    result *= a;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+// console.log(power(4, 3));
+// console.log(power(3, 10));
+
+
+const factorial = function (a) {
+  if (a == 0 || a == 1) return 1;
+  return a * factorial(a - 1);
 };
+
+console.log(factorial(10));
+
 
 // Do not edit below this line
 module.exports = {
