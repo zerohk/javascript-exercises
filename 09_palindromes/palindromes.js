2bf571d2fb2eae92588784bafaee66c76dfdbc8a
buercheng-linux <www.1245731267@qq.com>
2025-04-15 13:51:04 +0800

    finished 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  str = str.toUpperCase().split('').filter(
+    (char) => (char >= 'A' && char <= "Z") || (char >= '0' && char <= '9'));
+  return str.join('') === str.reverse().join('');
 };
+
+console.log("Animal loots foliated detail of stool lamina.".toUpperCase().split('').filter((char) =>
+  (char >= 'A' && char <= "Z") || (char >= '0' && char <= '9')).reverse().join('')
+);
+
+console.log(palindromes('racecar'));
+console.log(palindromes('Animal loots foliated detail of stool lamina.'));
+console.log(palindromes('ZZZZ car, a man, a maracaz.'));
+
+
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
