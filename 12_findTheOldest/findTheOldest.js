f1331c562d8b52a6c3afc4094fd14feab4e552cf
buercheng-linux <www.1245731267@qq.com>
2025-04-16 11:43:05 +0800

    finished all

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  let sortedPeople = people.sort((personA, personB) => {
+    let thisYear = new Date().getFullYear();
+    if (personA.yearOfDeath === undefined) {
+      personA.yearOfDeath = thisYear;
+    }
+    if (personB.yearOfDeath === undefined) {
+      personB.yearOfDeath = thisYear;
+    }
+    aAge = personA.yearOfDeath - personA.yearOfBirth;
+    bAge = personB.yearOfDeath - personB.yearOfBirth;
+    return bAge - aAge;
+  })
+  return sortedPeople[0];
 };
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
